dcd6a1903f3e804f12107c73952037cb23452ec1
Protocol Corporation <protocollive@outlook.com>
2022-07-19 12:21:44 -0300

    Using the new PhpLibeDb

--- src/Perms.php
+++ src/Perms.php
@@ -1,41 +1,33 @@
 <?php
-// Protocol Corporation Ltda.
-// https://github.com/ProtocolLive/PhpLive/
-// Version 2021.04.25.00
+//Protocol Corporation Ltda.
+//https://github.com/ProtocolLive
+//Version 2022.06.28.00
 
 class PhpLivePerms{
-  private PhpLivePdo $PhpLivePdo;
+  private PhpLiveDb $PhpLiveDb;
 
-  public function __construct(PhpLivePdo &$PhpLivePdo){
-    $this->PhpLivePdo = $PhpLivePdo;
+  public function __construct(PhpLiveDb &$PhpLiveDb){
+    $this->PhpLiveDb = $PhpLiveDb;
   }
 
-  public function Access(string $Resource, int $User = null, string $Site = null):array{
+  public function Access(string $Resource, int $User = null):array{
     $return = ['r' => false, 'w' => false, 'o' => false];
     //Get resource id
-    $result = $this->PhpLivePdo->BuildWhere([
-      ['site', $Site, PdoStr],
-      ['resource', $Resource, PdoStr]
-    ]);
-    $result = $this->PhpLivePdo->Run('
-      select resource_id
-      from sys_resources
-      where ' . $result['Query']
-    , $result['Tokens']);
+    $consult = $this->PhpLiveDb->Select('sys_resources');
+    $consult->Fields('resource_id');
+    $consult->WhereAdd('resource', $Resource, PhpLiveDbTypes::Str);
+    $result = $consult->Run();
     if(count($result) === 0):
       return $return;
     else:
-      $Resource = $result[0][0];
+      $Resource = $result[0]['resource_id'];
     endif;
     // Permissions for everyone
-    $result = $this->PhpLivePdo->Run('
-      select r,w,o
-      from sys_perms
-      where resource_id=?
-        and group_id=1
-    ',[
-      [1, $Resource, PdoInt]
-    ]);
+    $result = $this->PhpLiveDb->Select('sys_perms');
+    $result->Fields('r,w,o');
+    $result->WhereAdd('resource_id', $Resource, PhpLiveDbTypes::Int);
+    $result->WhereAdd('group_id', 1, PhpLiveDbTypes::Int);
+    $result = $result->Run();
     if(count($result) === 1):
       $return = $result[0];
     endif;
@@ -44,32 +36,47 @@ class PhpLivePerms{
       return $return;
     endif;
     // Admin?
-    $result = $this->PhpLivePdo->Run("
-      select *
-      from sys_usergroup
-      where group_id=3
-        and user_id=?
-    ",[
-      [1, $User, PdoInt]
-    ]);
+    $result = $this->PhpLiveDb->Select('sys_usergroup');
+    $result->WhereAdd('user_id', $User, PhpLiveDbTypes::Int);
+    $result->WhereAdd('group_id', 3, PhpLiveDbTypes::Int);
+    $result = $result->Run();
     if(count($result) === 1):
-      return ['r' => true, 'w' => true, 'o' => true];
+      return ['r' => true, 'w' => true, 'o' => false];
     endif;
     // Others
-    $result = $this->PhpLivePdo->Run("
-      select r,w,o
-      from sys_perms
-      where resource_id=:resource
-        and(
-          user_id=:user
-          or group_id=2
-          or group_id in (select group_id from sys_groups where user_id=:user)
-        )
-      order by r,w,o
-    ",[
-      [':resource', $Resource, PdoInt],
-      [':user', $User, PdoInt]
-    ]);
+    $result = $this->PhpLiveDb->Select('sys_perms');
+    $result->Fields('r,w,o');
+    $result->WhereAdd('resource_id', $Resource, PhpLiveDbTypes::Int);
+    $result->WhereAdd(
+      'user_id',
+      $User,
+      PhpLiveDbTypes::Int,
+      Parenthesis: PhpLiveDbParenthesis::Open
+    );
+    $result->WhereAdd(
+      'group_id',
+      'select group_id from sys_usergroup where user_id=:user_id',
+      PhpLiveDbTypes::Sql,
+      PhpLiveDbOperators::In,
+      PhpLiveDbAndOr::Or,
+      CustomPlaceholder: 'group1'
+    );
+    $result->WhereAdd(
+      'group_id',
+      1,
+      PhpLiveDbTypes::Int,
+      AndOr: PhpLiveDbAndOr::Or,
+      CustomPlaceholder: 'group2'
+    );
+    $result->WhereAdd(
+      'group_id',
+      2,
+      PhpLiveDbTypes::Int,
+      AndOr: PhpLiveDbAndOr::Or,
+      Parenthesis: PhpLiveDbParenthesis::Close,
+      CustomPlaceholder: 'group3'
+    );
+    $result = $result->Run();
     if(count($result) > 0):
       $return = $result[0];
     endif;
